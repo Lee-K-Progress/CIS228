9710a351dbf76733cdb6cb7ce95e2fa1cd6264b9
Lee-K-Progress <jkusowski7435@mail.kvcc.edu>
2026-09-23 22:24:36 -0400

    S2E6 Components Intro

--- practice/taskmate/src/App.js
+++ practice/taskmate/src/App.js
@@ -1,5 +1,28 @@
-export const App = () => {
-  return(
-    <h1>Welcome!</h1>
-  );
+import './App.css';
+
+//React Function
+export default function App() {
+  return (
+    <div>APP TEXT DISPLAY</div>
+  )
 }
+
+// //React Arrow Function
+// import React from 'react'
+
+// export const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// //React Class-based Component
+// import React, { Component } from 'react'
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>App</div>
+//     )
+//   }
+// }
